f7ad2c06927d1b54d4bec9e1e886a786231e3766
Nataliya Tahova <ntahova@gmail.com>
2024-05-28 21:21:01 +0200

    Cviceni-Realitka

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+export const Header = () =>{
+      return (
+            <header className="header">
+                  <nav>
+                        <ul>
+                              <li><a href="dum01.html">Dřevostavba v japonském stylu</a></li>
+                              <li><a href="dum02.html">Vilka na předměstí</a></li>
+                              <li><a href="dum03.html">Moderní dům s bazénem</a></li>
+                              <li><a href="dum04.html">Útulný domeček</a></li>
+                        </ul>
+                  </nav>
+            </header>
+
+      )
+
+}
